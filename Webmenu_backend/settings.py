333cec6c1187cf6a9393534a18ebb722ef493857
koocheyyy <116013985+koocheyyy@users.noreply.github.com>
2024-03-10 16:54:56 +0000

    Update settings.py

--- Webmenu_backend/settings.py
+++ Webmenu_backend/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
